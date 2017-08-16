ffb500e4e11f7f830bad7d0400f58157eadfe761
Gary Fleming <gazfleming@gmail.com>
2017-08-16 23:43:56 +0100

    Updated routes

--- routes.js
+++ routes.js
@@ -10,13 +10,11 @@ const dashboard = require('./controllers/dashboard.js');
 const trainerDashboard = require('./controllers/trainerDashboard.js'); //----> imports these objects
 const about = require('./controllers/about.js');
 const accounts = require('./controllers/accounts.js');
-const classes = require('./controllers/classes.js');
 
 // Links //
 
 router.get('/', accounts.index);
 router.get('/about', about.index);
-router.get('/classes', classes.index);
 router.get('/index', index.index); //----------------------------------------> matches the objects with each of these links
 router.get('/dashboard', dashboard.index);
 router.get('/trainerDashboard', trainerDashboard.index);
@@ -29,26 +27,31 @@ router.get('/logout', accounts.logout);
 router.post('/register', accounts.register);
 router.post('/authenticate', accounts.authenticate);
 
-// Dashboard //
-
+// Member Dashboard //
+router.get('/trainerDashboard/createclasses', trainerDashboard.createClasses);
 router.post('/dashboard/addassessment', dashboard.addAssessment);
 router.get('/dashboard/removeassessment/:assessmentid', dashboard.removeAssessment);
-router.get('/dashboard/allmemberclasses', dashboard.allMemberClasses);
-router.get('/settings', dashboard.settings);
-router.post('/settings', dashboard.updateProfile);
+router.get('/dashboard/memberclasses', dashboard.memberClasses);
 
 // Trainer Dashboard //
 
 router.get('/trainerDashboard/removemember/:memberid', trainerDashboard.removeMember);
 router.get('/trainerDashboard/viewassessments/:memberid', trainerDashboard.viewAssessments);
 router.get('/trainerDashboard/:memberid/removeassessment/:assessmentid', trainerDashboard.removeAssessment);
-router.post('/assessment/:memberid/updatecomment/:assessmentid', trainerDashboard.updateComment);
+router.post('/trainerDashboard/:memberid/updatecomment/:assessmentid', trainerDashboard.updateComment);
 
 router.post('/trainerDashboard/addclass', trainerDashboard.addClass);
-router.get('/trainerDashboard/allclasses', trainerDashboard.allClasses);
-router.get('/trainerDashboard/:trainerid/removeclass/:classid', trainerDashboard.removeClass);
+router.get('/trainerDashboard/viewtrainerclasses', trainerDashboard.viewTrainerClasses);
+router.get('/trainerDashboard/removeclass/:classid', trainerDashboard.removeClass);
 
-router.post('/trainerDashboard/editclass/', trainerDashboard.editClass); //TODO
-router.get('/trainerDashboard/editclass/:classid', trainerDashboard.viewEditClass); //TODO
+// Settings //
+
+router.get('/settings', dashboard.settings);
+router.post('/settings', dashboard.updateProfile);
+router.get('/classSettings', trainerDashboard.classSettings); //TODO
+router.post('/classSettings', trainerDashboard.updateClass); //TODO
+
+//router.get('/dashboard/enrollmember/:memberid/intoclass/:classid', dashboard.enrollMember);
+//router.get('/dashboard/unenroll/:memberid/fromclass/:classid', dashboard.unenrollMember);
 
 module.exports = router;
